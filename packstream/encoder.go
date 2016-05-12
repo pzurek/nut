f47f92b9998384df5d24466604da8b8ded494bfd
Piotr Zurek <p.zurek@gmail.com>
2016-05-12 14:06:09 +1200

    add fallthroughs

--- packstream/encoder.go
+++ packstream/encoder.go
@@ -32,12 +32,15 @@ func encodeInt(i int) (interface{}, error) {
 	case i >= MinTinyInt && i <= MaxTinyInt:
 		return byte(i), nil
 	case i < MinTinyInt && i >= math.MinInt8:
+		fallthrough
 	case i > MaxTinyInt && i <= math.MaxInt8:
 		return bytesFromInt(int8(i))
 	case i < math.MinInt8 && i >= math.MinInt16:
+		fallthrough
 	case i > math.MaxInt8 && i <= math.MaxInt16:
 		return bytesFromInt(int16(i))
 	case i < math.MinInt16 && i >= math.MinInt32:
+		fallthrough
 	case i > math.MaxInt16 && i <= math.MaxInt32:
 		return bytesFromInt(int32(i))
 	}
