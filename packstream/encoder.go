dd33414f044a0ea1dd7094c8435a3bb00fdaf863
Piotr Zurek <p.zurek@gmail.com>
2016-05-15 20:19:14 +1200

    some more encoding

--- packstream/encoder.go
+++ packstream/encoder.go
@@ -18,8 +18,8 @@ func NewEncoder(w io.Writer) *Encoder {
 	return &Encoder{w: w}
 }
 
-func (encoder *Encoder) Write(bytes ...byte) error {
-	n, err := encoder.w.Write(bytes)
+func (e *Encoder) write(bytes ...byte) error {
+	n, err := e.w.Write(bytes)
 	if n < len(bytes) {
 		return fmt.Errorf("failed to write all bytes")
 	}
@@ -29,22 +29,93 @@ func (encoder *Encoder) Write(bytes ...byte) error {
 	return nil
 }
 
-func bytesFromInt(i interface{}) ([]byte, error) {
+func (e *Encoder) encodeInt(i int) error {
+	// special casefor TinyInt
+	if i >= MinTinyInt && i <= MaxTinyInt {
+		return e.write(uint8(i)) // casting the byte to uint to let it overflow
+	}
+
+	b := []byte{}
+	var err error
+
+	switch {
+	case i < MinTinyInt && i >= math.MinInt8, i > MaxTinyInt && i <= math.MaxInt8:
+		b, err = numberToBytes(int8(i))
+	case i < math.MinInt8 && i >= math.MinInt16, i > math.MaxInt8 && i <= math.MaxInt16:
+		b, err = numberToBytes(int16(i))
+	case i < math.MinInt16 && i >= math.MinInt32, i > math.MaxInt16 && i <= math.MaxInt32:
+		b, err = numberToBytes(int32(i))
+	default:
+		b, err = numberToBytes(int64(i))
+	}
+	if err != nil {
+		return err
+	}
+	err = e.write(b...)
+	return err
+}
+
+func (e *Encoder) encodeNull() error {
+	return e.write(Null)
+}
+
+func (e *Encoder) encodeBool(v bool) error {
+	if v {
+		return e.write(True)
+	}
+	return e.write(False)
+}
+
+func (e *Encoder) encodeFloat64(f float64) error {
+	b, err := numberToBytes(f)
+	if err != nil {
+		return err
+	}
+	return e.write(b...)
+}
+
+func (e *Encoder) encodeString(s string) error {
+	return e.write([]byte(s)...)
+}
+
+// numberToBytes is a little wrapper around binary.Write
+// it takes any fixed size number (int8, int64, float64 etc, but not int)
+// and returns BigEndianbytes
+func numberToBytes(n interface{}) ([]byte, error) {
 	b := &bytes.Buffer{}
-	err := binary.Write(b, binary.BigEndian, i)
+	err := binary.Write(b, binary.BigEndian, n)
 	return b.Bytes(), err
 }
 
-func encodeInt(i int) (interface{}, error) {
-	switch {
-	case i >= MinTinyInt && i <= MaxTinyInt:
-		return byte(i), nil
-	case i < MinTinyInt && i >= math.MinInt8, i > MaxTinyInt && i <= math.MaxInt8:
-		return bytesFromInt(int8(i))
-	case i < math.MinInt8 && i >= math.MinInt16, i > math.MaxInt8 && i <= math.MaxInt16:
-		return bytesFromInt(int16(i))
-	case i < math.MinInt16 && i >= math.MinInt32, i > math.MaxInt16 && i <= math.MaxInt32:
-		return bytesFromInt(int32(i))
+func (e *Encoder) encodeStringHeader(s string) error {
+	l := len([]byte(s))
+	return e.write(byte(l))
+}
+
+func (e *Encoder) encodeTinyIntHeader(i int) error {
+	b := uint8(i)
+	return e.write(b)
+}
+
+func (e *Encoder) encodeInt8Header() error {
+	return e.write(Int8)
+}
+
+func (e *Encoder) encodeInt16Header() error {
+	return e.write(Int16)
+}
+
+func (e *Encoder) encodeInt32Header() error {
+	return e.write(Int32)
+}
+
+func (e *Encoder) encodeInt64Header() error {
+	return e.write(Int64)
+}
+
+func (e *Encoder) encodeBoolHeader(b bool) error {
+	if b {
+		return e.write(True)
 	}
-	return bytesFromInt(int64(i))
+	return e.write(False)
 }
